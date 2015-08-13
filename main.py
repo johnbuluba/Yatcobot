a637458e86313bed5aae38f3132c6fd61c647eeb
Conna Wiles <kurozael@gmail.com>
2015-08-13 09:50:16 +0000

    Added some comments, retweet_update_time config, and moved CheckForFollowRequest to UpdateQueue (rate limit protection.)

--- main.py
+++ main.py
@@ -2,19 +2,24 @@ from TwitterAPI import TwitterAPI
 import threading
 import time
 
+# Fill these in with your Twitter API auth details.
 consumer_key = "YOUR CONSUMER KEY"
 consumer_secret = "YOUR CONSUMER SECRET"
 access_token_key = "YOUR ACCESS TOKEN KEY"
 access_token_secret = "YOUR ACCESS TOKEN SECRET"
 
+# How many seconds have to pass for 1 retweet/follow to be made?
+# The higher the number, the less retweets will be made in a day.
+retweet_update_time = 5.0
+
+# Don't edit these unless you know what you're doing.
 api = TwitterAPI(consumer_key, consumer_secret, access_token_key, access_token_secret)
-
 post_list = list()
-
 last_twitter_id = 0
 
+# Update the Retweet queue (this prevents too many retweets happening at once.)
 def UpdateQueue():
-    u = threading.Timer(5.0, UpdateQueue)
+    u = threading.Timer(retweet_update_time, UpdateQueue)
     u.daemon = True;
     u.start()
 
@@ -22,11 +27,15 @@ def UpdateQueue():
 
     if len(post_list) > 0:
         post = post_list[0]
-        print("Retweeting: " + str(post))
-
-        api.request('statuses/retweet/:' + str(post))
+        print("Retweeting: " + str(post['id']))
+        
+        CheckForFollowRequest(post)
+        
+        api.request('statuses/retweet/:' + str(post['id']))
         post_list.pop(0)
 
+
+# Check if a post requires you to follow the user.
 def CheckForFollowRequest(item):
     text = item['text']
     if "follow" in text.lower():
@@ -35,6 +44,7 @@ def CheckForFollowRequest(item):
         api.request('friendships/create', {'screen_name': screen_name})
 
 
+# Scan for new contests, but not too often because of the rate limit.
 def ScanForContests():
     t = threading.Timer(10.0, ScanForContests)
     t.daemon = True;
@@ -51,8 +61,7 @@ def ScanForContests():
             if (item['id'] > last_twitter_id):
                 last_twitter_id = item['id']
 
-            post_list.append(item['id'])
-            CheckForFollowRequest(item)
+            post_list.append(item)
             print(item)
 
 
