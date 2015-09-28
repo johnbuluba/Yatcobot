2c11286494e122b23c40f7540181b5dc49517cb5
John Buluba <buluba89@gmail.com>
2015-09-28 16:08:19 +0300

    Fixed fixtures path

--- tests/test_twitterclient.py
+++ tests/test_twitterclient.py
@@ -1,4 +1,4 @@
-import json
+import os
 import unittest
 import logging
 
@@ -12,12 +12,12 @@ class TestTwitterClient(unittest.TestCase):
 
     def setUp(self):
         logging.disable(logging.ERROR)
-
+        self.tests_path = path = os.path.dirname(os.path.abspath(__file__))
         self.client = TwitterClient('Consumer Key', "Consumer Secret", "Access Key", "Access Secret")
 
     @requests_mock.mock()
     def test_get_friends_ids(self, m):
-        with open('fixtures/friends_ids.json') as f:
+        with open(self.tests_path + '/fixtures/friends_ids.json') as f:
             response = f.read()
         m.get('https://api.twitter.com/1.1/friends/ids.json', text=response)
         r = self.client.get_friends_ids()
