a55d84c48920cda5581e7f544fb9f465ca36a307
John Buluba <buluba89@gmail.com>
2015-09-28 16:34:55 +0300

    added test for search tweets

--- tests/test_twitterclient.py
+++ tests/test_twitterclient.py
@@ -16,6 +16,15 @@ class TestTwitterClient(unittest.TestCase):
         self.client = TwitterClient('Consumer Key', "Consumer Secret", "Access Key", "Access Secret")
 
     @requests_mock.mock()
+    def test_search_tweets(self, m):
+        with open(self.tests_path + '/fixtures/search_tweets.json') as f:
+            response = f.read()
+        m.get('https://api.twitter.com/1.1/search/tweets.json?q=210462857140252672&result_type=mixed&count=50',
+                                                                                                    text=response)
+        r = self.client.search_tweets("210462857140252672", 50)
+        self.assertEqual(len(r), 4)
+
+    @requests_mock.mock()
     def test_get_tweet(self, m):
         with open(self.tests_path + '/fixtures/statuses_show.json') as f:
             response = f.read()
@@ -30,6 +39,8 @@ class TestTwitterClient(unittest.TestCase):
         m.get('https://api.twitter.com/1.1/friends/ids.json', text=response)
         r = self.client.get_friends_ids()
         self.assertEqual(len(r), 31)
+
+
 
 
 
