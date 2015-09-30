a765a111d80c3b6bfd3b4aa3aeb9d984e539a01d
John Buluba <buluba89@gmail.com>
2015-09-30 23:38:37 +0300

    fixed update error

--- tests/test_bot.py
+++ tests/test_bot.py
@@ -14,6 +14,7 @@ class TestBot(unittest.TestCase):
     @patch('yatcobot.bot.IgnoreList')
     @patch('yatcobot.bot.Config')
     def setUp(self, config_mock, ignore_list_mock, client_mock):
+        self.config = config_mock
         self.bot = Yatcobot('test', 'test')
 
     def test_get_original_tweet_no_retweet(self):
@@ -34,9 +35,9 @@ class TestBot(unittest.TestCase):
         self.assertFalse(self.bot.post_list.popitem.called)
 
     def test_clear_queue_full(self):
-        Config.max_queue = 60
+        self.config.max_queue = 60
         self.bot.post_list = MagicMock()
-        self.bot.post_list.__len__.return_value = Config.max_queue + 1
+        self.bot.post_list.__len__.return_value = self.config.max_queue + 1
 
         self.bot.clear_queue()
         self.assertTrue(self.bot.post_list.popitem.called)
