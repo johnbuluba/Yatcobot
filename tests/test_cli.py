4ea424c18fe5c95455b5fb5436c1537a9119e31b
John Buluba <buluba89@gmail.com>
2015-10-02 20:56:03 +0300

    Added test for cli

---
+++ tests/test_cli.py
@@ -0,0 +1,68 @@
+import unittest
+import logging
+import sys
+
+from unittest.mock import MagicMock
+
+import yatcobot.cli
+from yatcobot.cli import main
+
+
+
+logging.disable(logging.ERROR)
+
+
+class TestCli(unittest.TestCase):
+
+    program_name = 'yatcobot.py'
+
+    def setUp(self):
+        yatcobot.cli.Yatcobot = MagicMock()
+        yatcobot.cli.Config = MagicMock()
+
+    def test_simple_start(self):
+        sys.argv = [self.program_name]
+
+        main()
+
+        yatcobot.cli.Config.load.assert_called_once_with('config.json')
+        yatcobot.cli.Yatcobot.assert_called_once_with('ignorelist')
+        self.assertTrue(yatcobot.cli.Yatcobot.return_value.run.called)
+
+    def test_simple_select_config(self):
+        sys.argv = [self.program_name, '--config', 'test.json']
+
+        main()
+
+        yatcobot.cli.Config.load.assert_called_once_with('test.json')
+        yatcobot.cli.Yatcobot.assert_called_once_with('ignorelist')
+        self.assertTrue(yatcobot.cli.Yatcobot.return_value.run.called)
+
+    def test_simple_select_ignorelist(self):
+        sys.argv = [self.program_name, '--ignore_list', 'test']
+
+        main()
+
+        yatcobot.cli.Config.load.assert_called_once_with(('config.json'))
+        yatcobot.cli.Yatcobot.assert_called_once_with('test')
+        self.assertTrue(yatcobot.cli.Yatcobot.return_value.run.called)
+
+    def test_simple_select_log(self):
+        sys.argv = [self.program_name, '--log', 'test']
+        yatcobot.cli.create_logger = MagicMock()
+        main()
+
+        yatcobot.cli.Config.load.assert_called_once_with(('config.json'))
+        yatcobot.cli.Yatcobot.assert_called_once_with('ignorelist')
+        yatcobot.cli.create_logger.assert_called_once_with(logging.INFO, 'test')
+        self.assertTrue(yatcobot.cli.Yatcobot.return_value.run.called)
+
+    def test_simple_debug(self):
+        sys.argv = [self.program_name, '--debug']
+        yatcobot.cli.create_logger = MagicMock()
+        main()
+
+        yatcobot.cli.Config.load.assert_called_once_with(('config.json'))
+        yatcobot.cli.Yatcobot.assert_called_once_with('ignorelist')
+        yatcobot.cli.create_logger.assert_called_once_with(logging.DEBUG, None)
+        self.assertTrue(yatcobot.cli.Yatcobot.return_value.run.called)
