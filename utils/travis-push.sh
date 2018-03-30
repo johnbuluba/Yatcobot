aa740d5c50e8f693df48cd9b45203eeb6ff6c09c
John Buluba <buluba89@gmail.com>
2018-03-31 01:24:54 +0300

    travis trigger docker buil

--- utils/travis-push.sh
+++ utils/travis-push.sh
@@ -12,3 +12,5 @@ setup_git
 bumpversion --no-tag --message '[ci skip] Travis bump version: {current_version} → {new_version}' patch
 
 git push --tags "https://${GH_TOKEN}@github.com/buluba89/Yatcobot.git" $TRAVIS_BRANCH
+
+curl -H "Content-Type: application/json" --data '{"source_type": "$TRAVIS_BRANCH", "source_name": "staging"}' -X POST https://registry.hub.docker.com/u/buluba89/yatcobot/trigger/$DH_TOKEN/
