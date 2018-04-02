daa428a106e4913bea4a1683b67b1c80f07bff40
Travis CI <travis@travis-ci.org>
2018-04-02 03:48:19 +0000

    [ci skip] Travis bump version: 2.2.11 → 2.2.12

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(path.join(root_path, 'README.rst'), encoding='utf-8') as f:
 setup(
     name='Yatcobot',
 
-    version='2.2.11',
+    version='2.2.12',
 
     description='The best bot for searching twitter contests and automatically retweet them',
 
